6916bce6600ad5250b84034f88dae1cbf974a3df
NicoSam <nicolas.sammarco@gmail.com>
2021-02-13 19:40:28 -0300

    completo

---
+++ assets/img/avatars12-02-pm-18-36-19-DbConnection.php
@@ -0,0 +1,26 @@
+<?php
+class conexionDb{
+
+    private $host = 'localhost';
+    private $usuario = 'root';
+    private $password = '';
+    private $database = 'gestion_thi';
+    
+    protected $conexion;
+    
+    public function __construct(){
+
+        if (!isset($this->conexion)) {
+            
+            $this->conexion = new mysqli($this->host, $this->usuario, $this->password, $this->database);
+            
+            if (!$this->conexion) {
+                echo 'Error al conectar a la base de datos';
+                exit;
+            }            
+        }    
+        
+        return $this->conexion;
+    }
+}
+?>
