7ee0cebcd50c09fdb1224761e852e3f937b94748
NicoPCEscritorio <nsammarco@baricode.com>
2022-02-03 16:32:42 -0300

    arreglar para exportar archivos

--- Lista_dedicacion.php
+++ Lista_dedicacion.php
@@ -26,20 +26,10 @@ if (isset($_GET['borrarid']) && !empty($_GET['borrarid'])) {
 }
 //carga los datos cuando recien entra a la pagina
 $query = "SELECT * FROM dedicacion ";
-//bandera para que desaparezca boton volver a la lista
-$flag = false;
 
-//carga la consulta de busqueda
-if (isset($_POST['submit'])) {
-    $query = "SELECT * FROM dedicacion WHERE mes LIKE '%" . $_POST['busqueda'] . "%'";
-    $flag = true;
-}
-//es lo que imprime al inicio.
-if (isset($_POST['volver'])) {
 
-    $query = "SELECT * FROM dedicacion ";
-    $flag = false;
-}
+
+
 
 ?>
 <!DOCTYPE html>
@@ -48,13 +38,26 @@ if (isset($_POST['volver'])) {
 <head>
     <title>Mi Dedicacion</title>
     <?php include('header.php'); ?>
-    <link href="https://cdn.jsdelivr.net/npm/jquery-treegrid@0.3.0/css/jquery.treegrid.css" rel="stylesheet">
+    <!-- no anda el exportar archivos -->
     <link href="https://unpkg.com/bootstrap-table@1.19.1/dist/bootstrap-table.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/jquery-treegrid@0.3.0/js/jquery.treegrid.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.19.1/dist/bootstrap-table.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.19.1/dist/extensions/treegrid/bootstrap-table-treegrid.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-table@1.19.1/dist/bootstrap-table-locale-all.min"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/tableExport.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF/jspdf.min.js"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF-AutoTable/jspdf.plugin.autotable.js"></script>
 </head>
 
+<script>
+    $(document).ready(function() {
+        $(document).on('change', '#selectormes', function() {
+            alert("The text has been changed.");
+        });
+
+
+    });
+</script>
 
 <body id="page-top">
     <div id="wrapper">
@@ -123,79 +126,91 @@ if (isset($_POST['volver'])) {
                         </div>
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-md-6 text-nowrap">
-                                    <!-- <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label>Mostrar&nbsp;<select class="form-control form-control-sm custom-select custom-select-sm"><option value="10" selected="">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select>&nbsp;</label></div> -->
-                                    <?php
-                                    if ($flag == true) { ?>
-                                        <form method="POST" action="Lista_dedicacion.php">
-                                            <input style="display: none;" type="text" name="busqueda" id="volver">
-                                            <input type="submit" name="volver" class="btn btn-info btn-sm" value="Volver a la lista completa" />
+                                <div class="col-md-3 text-nowrap">
+                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label>Mostrar&nbsp;<select class="form-control form-control-sm custom-select custom-select-sm">
+                                                <option value="5" selected="">5</option>
+                                                <option value="10">10</option>
+                                                <option value="20">20</option>
+                                                <option value="100">Todos</option>
+                                            </select>&nbsp;</label></div>
 
-                                        </form>
-                                    <?php }
-                                    $flag = false; ?>
+                                    
                                 </div>
-                                <div class="col-md-6">
-                                    <form method="POST" action="Lista_dedicacion.php">
-                                        <div class="row">
-                                            <div class="col" style="max-width: fit-content; margin-right: 0px; margin-left: auto;">
-                                                <input type="text" name="busqueda" id="busqueda" placeholder="Ingrese nombre">
-                                            </div>
-                                            <div class="col" style="padding: 0px; max-width: fit-content;">
-                                                <input type="submit" name="submit" class="btn btn-primary btn-sm" value="Buscar" />
-                                            </div>
-                                        </div>
-                                    </form>
+                                <div class="col-md-3" style=" margin-left: 0px; margin-right: auto;">
+                                    <select class="form-control form-control-sm" id="selectormes">
+                                        <option selected>Filtrar por mes</option>
+                                        <option value="1">Enero</option>
+                                        <option value="2">Febrero</option>
+                                        <option value="3">Marzo</option>
+                                        <option value="4">Abril</option>
+                                        <option value="5">Mayo</option>
+                                        <option value="6">Junio</option>
+                                        <option value="7">Julio</option>
+                                        <option value="8">Agosto</option>
+                                        <option value="9">Septiembre</option>
+                                        <option value="10">Octubre</option>
+                                        <option value="11">Noviembre</option>
+                                        <option value="12">Diciembre</option>
+                                    </select>
                                 </div>
-                            </div>
-                            <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                                <table class="table dataTable my-0" id="dataTable">
-                                    <thead class="thead-dark">
-                                        <tr>
-
-                                            <th>Mes</th>
-                                            <th>Horas</th>
-                                            <th>Imputación</th>
-                                            <th>Editar</th>
-                                            <th>Eliminar</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
-                                        <?php
-                                        $filas = $dedicacion->mostrarDatosBusqueda($query);
-                                        foreach ($filas as $fila) {
-                                        ?>
+                               
+  
+                                <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                                    <table class="table dataTable my-0" id="table"                                    
+                                    data-show-export="true" 
+                                    data-force-export="true"
+                                    data-toggle="table" 
+                                     data-search="true"
+                                     data-pagination="true"
+                                      data-show-columns="true"
+                                      data-locale="es-ES">
+                                        <thead class="thead-dark">
                                             <tr>
-                                                <?php $aux_p = $proyecto->mostrarFilaPorId($fila["imputacion"]);
-                                               // $aux_u = $user->mostrarFilaPorId($fila["id_agente"]);//aca tengo el nombre del usuario
-                                                ?>
-
-                                                <td><?php echo $fila['mes'] ?></td>
-                                                <td><?php echo $fila['horas'] ?></td>
-                                                <td><?php echo $aux_p['nombre'] ?></td>
-
-                                                <script src="cartel.js"> </script>
-                                                <!-- <td><a class="btn btn-primary mx-auto btn-circle ml-1"  role="button" href="crear_tarea.php?tareaId=<?php //echo $fila["id_proyectos"]; 
-                                                                                                                                                            ?>"><i class="fas fa-file-medical text-white"></i></a></td> -->
-
-                                                <td><a class="btn btn-info mx-auto btn-circle ml-1" role="button" href="actualizar_dedicacion.php?editId=<?php echo $fila['id_dedicacion'] ?>"><i class="fa fa-edit text-white"></i></a></td>
-                                                <td><a class="btn btn-danger mx-auto btn-circle ml-1" onclick="return confirmBorrar()" role="button" href="Lista_dedicacion.php?borrarid=<?php echo $fila['id_dedicacion'] ?>"><i class="fas fa-trash text-white"></i></a></td>
-
+                                                <th data-field="Mes">Mes</th>
+                                                <th data-field="Horas P">Horas planificadas</th>
+                                                <th data-field="Horas R">Horas relevadas</th>
+                                                <th data-field="Imputacion">Imputación</th>
+                                                <th data-field="Editar">Editar</th>
+                                                <th data-field="Borrar"> Eliminar</th>
                                             </tr>
-                                        <?php }  ?>
+                                        </thead>
+                                        <tbody id="tablaDedicacion">
+
+                                            <?php
+                                            $filas = $dedicacion->mostrarDatosBusqueda($query);
+                                            foreach ($filas as $fila) {
+                                            ?>
+                                                <tr>
+                                                    <?php $aux_p = $proyecto->mostrarFilaPorId($fila["imputacion"]);
+                                                    // $aux_u = $user->mostrarFilaPorId($fila["id_agente"]);//aca tengo el nombre del usuario
+                                                    ?>
+
+                                                    <td><?php echo $fila['mes'] ?></td>
+                                                    <td class="centrarRegistros"><?php echo $fila['horas'] ?></td>
+                                                    <td><?php echo $fila['horas'] ?></td>
+                                                    <td><?php echo $aux_p['nombre'] ?></td>
+
+                                                    <script src="cartel.js"> </script>
+                                                    <!-- <td><a class="btn btn-primary mx-auto btn-circle ml-1"  role="button" href="crear_tarea.php?tareaId=<?php //echo $fila["id_proyectos"]; 
+                                                                                                                                                                ?>"><i class="fas fa-file-medical text-white"></i></a></td> -->
+
+                                                    <td><a class="btn btn-info mx-auto btn-circle ml-1" role="button" href="actualizar_dedicacion.php?editId=<?php echo $fila['id_dedicacion'] ?>"><i class="fa fa-edit text-white"></i></a></td>
+                                                    <td><a class="btn btn-danger mx-auto btn-circle ml-1" onclick="return confirmBorrar()" role="button" href="Lista_dedicacion.php?borrarid=<?php echo $fila['id_dedicacion'] ?>"><i class="fas fa-trash text-white"></i></a></td>
+
+                                                </tr>
+                                            <?php }  ?>
 
 
-                                    </tbody>
+                                        </tbody>
 
-                                </table>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 align-self-center">
-                                    <!-- <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Mostrando 1 al 10 de 27</p> -->
-
+                                    </table>
                                 </div>
-                                <!-- <div class="col-md-6"> 
+                                <div class="row">
+                                    <div class="col-md-6 align-self-center">
+                                        <!-- <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Mostrando 1 al 10 de 27</p> -->
+
+                                    </div>
+                                    <!-- <div class="col-md-6"> 
                                 <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
                                     <ul class="pagination">
                                         <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
@@ -206,14 +221,16 @@ if (isset($_POST['volver'])) {
                                     </ul>
                                 </nav>
                             </div>-->
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
+        </div>
+        
 </body>
 <?php include('footer.php'); ?>
 
--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-    <script type="text/javascript" src="js/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+<link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/Chart.min.js"></script>
